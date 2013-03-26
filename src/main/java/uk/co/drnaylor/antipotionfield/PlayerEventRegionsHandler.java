38a73ab0ae60d94b138105c38c3f92f24a026f13
Eevee Modevet <Eevables@hotmail.com>
2013-03-26 10:11:57 -0500

    Added some comments and removed others.
    It needed some cleanup.

--- src/main/java/uk/co/drnaylor/antipotionfield/PlayerEventRegionsHandler.java
+++ src/main/java/uk/co/drnaylor/antipotionfield/PlayerEventRegionsHandler.java
@@ -17,24 +17,33 @@ import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 
 public class PlayerEventRegionsHandler implements Listener {
-
+	
+	/**
+	 * Checks the player's current effects to see if they are allowed at their location.
+	 * 
+	 * @param event The PlayerMoveEvent involving the player.
+	 */
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerMove(PlayerMoveEvent event) {
 		final Player player = event.getPlayer();
 		//See the Util class
-		if (!Util.canUsePotions(player)) {
+		if (!Util.canUsePotions(player)) { // Will be changed to hasDeniedEffects(player) later.
 			Util.removePositiveEffects(player);
-
-			//player.removePotionEffect(PotionEffectType.SLOW);
+			
 		}
 	}
-
+	
+	/**
+	 * Checks to see if the player is attempting to drink a potion, and checks the potion to see if it is allowed.
+	 * 
+	 * @param event The PlayerInteractEvent involving the player.
+	 */
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if (event.getPlayer().getItemInHand().getType() == Material.POTION) {
 			if ((event.getAction() == Action.RIGHT_CLICK_AIR) || (event.getAction() == Action.RIGHT_CLICK_BLOCK) ) {
 				
-				// Check the type of potion in the player's hand right here
+				// Check the type of potion in the player's hand
 				Potion potion = Potion.fromItemStack(event.getPlayer().getItemInHand());
 				ArrayList <PotionEffect> effects = (ArrayList <PotionEffect>) potion.getEffects();
 				
@@ -45,7 +54,6 @@ public class PlayerEventRegionsHandler implements Listener {
 					}
 				}
 				
-				//if (!Util.canUsePotions(event.getPlayer())) { // Add the potion type as an argument for checking
 				if (cancelEvent) {
 					event.setCancelled(true);
 					event.getPlayer().sendMessage(ChatColor.RED + "You cannot use that potion here!");
