9268d9da78c7b84ab3992b5a83cebe525675ba16
Daniel Naylor <git@drnaylor.co.uk>
2013-03-29 00:09:17 +0000

    Adding EffectMeans enum

--- src/main/java/uk/co/drnaylor/antipotionfield/Util.java
+++ src/main/java/uk/co/drnaylor/antipotionfield/Util.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import uk.co.drnaylor.antipotionfield.enums.EffectMeans;
 import uk.co.drnaylor.antipotionfield.worldguardapi.WorldGuardAPIException;
 import uk.co.drnaylor.antipotionfield.worldguardapi.WorldGuardInterface;
 
